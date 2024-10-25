15b5a9aeeb2c69f30ad61e2101ac4780930e7884
AndyKodehode <andykodehode@gmail.com>
2024-10-25 19:33:37 +0200

    begynt på kommentarer for template

---
+++ script.js
@@ -0,0 +1,39 @@
+
+//HTML
+
+/*
+tite: space-invaders
+div med class "grid"
+koble til script
+tekst element for results
+
+
+
+*/
+
+//css
+
+/*
+
+sette en bredde og høyde for grid
+sette en høyde og bredde for divs til grid
+sette flex slik at boksene dekker grid både horisontalt og vertikalt
+
+*/
+
+
+//JS
+
+/*
+ting vi vil jobbe med i dette prosjektet
+- Arrays
+- Loops
+
+sette hvor mange divs du vil ha i bredden, 
+det må være et tall som går opp med bredden og høyden du har satt på div og grid
+Lage en loop som genererer firkanter som dekker hele grid
+
+Du må samle alle divs på en plass
+
+
+*/
