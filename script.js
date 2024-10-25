5d9753750c76ed27ad6057075f3a686763e0d7ef
AndyKodehode <andykodehode@gmail.com>
2024-10-26 00:39:20 +0200

    flere instrukser

--- script.js
+++ script.js
@@ -19,6 +19,10 @@ sette en bredde og høyde for grid
 sette en høyde og bredde for divs til grid
 sette flex slik at boksene dekker grid både horisontalt og vertikalt
 
+lage en class for invaders
+lage en class for shooter
+lage en class for laser
+
 */
 
 
@@ -28,12 +32,35 @@ sette flex slik at boksene dekker grid både horisontalt og vertikalt
 ting vi vil jobbe med i dette prosjektet
 - Arrays
 - Loops
+- e.key
 
 sette hvor mange divs du vil ha i bredden, 
 det må være et tall som går opp med bredden og høyden du har satt på div og grid
 Lage en loop som genererer firkanter som dekker hele grid
 
-Du må samle alle divs på en plass
+Samle alle divs i en liste
+
+Lag en liste over aliens og legge de til grid
+
+Invaders må bli borte når man treffer de
+
+du må legge til shooter på brettet
+
+Finn en måte å få shooter til å bevege seg når du trykker på enkelte taster
+
+du må lage en sjekk for om man er på endene av brettet slik at du ikke havner på andre siden
+
+Få invaders til å bevege på seg
+
+Du må sette en interval på hvor ofte/fort invaders kan bevege seg
+
+når invaders treffer vegg så må den gå ned et hakk og skifte retning
+
+du må lage laser som treffer invaders, få den til å bevege seg
+
+hva skjer når invaders treffer shooter?
+
+når vinner man og hva skjer da?
 
 
 */
