785cbbfe0000ea556643b12e8837fb7f55cf2a39
AndyKodehode <andykodehode@gmail.com>
2024-10-26 10:13:55 +0200

    sortert instrukser

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ div med class "grid"
 koble til script
 tekst element for results
 
-
-
 */
 
 //css
@@ -33,6 +31,12 @@ ting vi vil jobbe med i dette prosjektet
 - Arrays
 - Loops
 - e.key
+- classList.remove
+- classList.add
+- if statement
+
+
+//Grid//
 
 sette hvor mange divs du vil ha i bredden, 
 det må være et tall som går opp med bredden og høyden du har satt på div og grid
@@ -42,13 +46,15 @@ Samle alle divs i en liste
 
 Lag en liste over aliens og legge de til grid
 
-Invaders må bli borte når man treffer de
-
 du må legge til shooter på brettet
+
+//Bevegelse//
 
 Finn en måte å få shooter til å bevege seg når du trykker på enkelte taster
 
 du må lage en sjekk for om man er på endene av brettet slik at du ikke havner på andre siden
+
+//Invaders//
 
 Få invaders til å bevege på seg
 
@@ -56,7 +62,13 @@ Du må sette en interval på hvor ofte/fort invaders kan bevege seg
 
 når invaders treffer vegg så må den gå ned et hakk og skifte retning
 
-du må lage laser som treffer invaders, få den til å bevege seg
+//Laser//
+
+du må lage laser som treffer invaders, få laser  til å bevege seg
+
+Invaders må bli borte når man treffer de
+
+//Regler og resultat//
 
 hva skjer når invaders treffer shooter?
 
